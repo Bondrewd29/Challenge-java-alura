6f73804bc1471e31c21c6c386d510a40050084e0
kevyn <kevmora29@hotmail.com>
2023-03-21 17:42:06 -0600

    primera actualizacion de codigo

--- src/Proyecto.java
+++ src/Proyecto.java
@@ -1,6 +1,9 @@
+import javax.swing.JOptionPane;
+
 
 public class Proyecto {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		Object[] option = {"Convertir divisas", "Convertir temperatura", "Salir"};
 		
 	}
 }
